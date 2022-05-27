870836468ac8fdc870e229415a2272a7d1b43d0e
JwaSangHeon <whk4051@naver.com>
2022-05-27 17:57:46 +0900

    bindActionCreators

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -2,6 +2,7 @@ import Counter from "../components/Counter";
 import { connect } from "react-redux";
 import React from "react";
 import { increase, decrease } from "../modules/counter";
+import { bindActionCreators } from "redux";
 
 const CounterContainer = ({ number, increase, decrease }) => {
   return (
@@ -9,17 +10,18 @@ const CounterContainer = ({ number, increase, decrease }) => {
   );
 };
 
-const mapStateToProps = (state) => ({
-  number: state.counter.number,
-});
-const mapDispatchToProps = (dispatch) => ({
-  //임시 함수
-  increase: () => {
-    dispatch(increase());
-  },
-  decrease: () => {
-    dispatch(decrease());
-  },
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+export default connect(
+  (state) => ({ number: state.counter.number }),
+  (dispatch) =>
+    bindActionCreators(
+      {
+        increase,
+        decrease,
+      },
+      dispatch
+    )
+    // {
+    // increase: () => dispatch(increase()),
+    // decrease: () => dispatch(decrease()),
+    // }
+)(CounterContainer);
