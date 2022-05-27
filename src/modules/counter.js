6c1637458e10408a1df24efc715fd1de5944c68d
JwaSangHeon <whk4051@naver.com>
2022-05-27 20:17:17 +0900

    redux-actions 사용 - 리팩토링

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,10 +1,12 @@
+import { createAction, handleActions } from "redux-actions";
+
 // action
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
 
 // action 생성 함수
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 // 초기 state
 const initalState = {
@@ -12,19 +14,12 @@ const initalState = {
 };
 
 // reducer
-function counter(state = initalState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initalState
+);
 
 export default counter;
--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,16 +1,20 @@
+import createAction from "redux-actions/lib/createAction";
+import handleActions from "redux-actions/lib/handleActions";
+
 const CHANGE_INPUT = "todos/CHANGE_INPUT"; //input 값 변경
 const INSERT = "todos/INSERT"; // 새로운 todo를 등록
 const TOGGLE = "todos/TOGGLE"; // todo를 체크/체크 해제
 const REMOVE = "todos/REMOVE"; // todo를 제거
 
-export const changeInput = (input) => ({ type: CHANGE_INPUT, input });
+export const changeInput = createAction(CHANGE_INPUT, (input) => input);
 let id = 3; //insert 호출시 1씩 더해진다.
-export const insert = (text) => ({
-  type: INSERT,
-  todo: { id: id++, text, done: false },
-});
-export const toggle = (id) => ({ type: TOGGLE, id });
-export const remove = (id) => ({ type: REMOVE, id });
+export const insert = createAction(INSERT, (text) => ({
+  id: id++,
+  text,
+  done: false,
+}));
+export const toggle = createAction(TOGGLE, (id) => id);
+export const remove = createAction(REMOVE, (id) => id);
 
 const initalState = {
   input: "",
@@ -28,33 +32,25 @@ const initalState = {
   ],
 };
 
-function todos(state = initalState, action) {
-  switch (action.type) {
-    case CHANGE_INPUT:
-      return {
-        ...state,
-        input: action.input,
-      };
-    case INSERT:
-      return {
-        ...state,
-        todos: state.todos.concat(action.todo),
-      };
-    case TOGGLE:
-      return {
-        ...state,
-        todos: state.todos.map((todo) =>
-          todo.id === action.id ? { ...todo, done: !todo.done } : todo
-        ),
-      };
-    case REMOVE:
-      return {
-        ...state,
-        todos: state.todos.filter((todo) => todo.id !== action.id),
-      };
-    default:
-      return state;
-  }
-}
+const todos = handleActions(
+  {
+    [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input: input }),
+    [INSERT]: (state, { payload: todo }) => ({
+      ...state,
+      todos: state.todos.concat(todo),
+    }),
+    [TOGGLE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.map((todo) =>
+        todo.id === id.payload ? { ...todo, done: !todo.done } : todo
+      ),
+    }),
+    [REMOVE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.filter((todo) => todo.id !== id),
+    }),
+  },
+  initalState
+);
 
 export default todos;
