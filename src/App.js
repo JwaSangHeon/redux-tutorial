44b3f4bceebaccb364befa2cf21581a8e03d9299
JwaSangHeon <whk4051@naver.com>
2022-05-27 18:47:14 +0900

    기능 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Todos from "./components/Todos";
 import CounterContainer from "./containers/CounterContainer";
+import TodosContainer from "./containers/TodosContainer";
 
 function App() {
   return (
     <>
       <CounterContainer />
       <hr />
-      <Todos />
+      <TodosContainer />
     </>
   );
 }
