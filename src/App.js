53387540cd95782f7363b2fcc07949c25079da4c
JwaSangHeon <whk4051@naver.com>
2022-05-27 17:51:55 +0900

    counter redux 연결

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Counter from "./components/Counter";
 import Todos from "./components/Todos";
+import CounterContainer from "./containers/CounterContainer";
 
 function App() {
   return (
     <>
-      <Counter number={0} />
+      <CounterContainer />
       <hr />
       <Todos />
     </>
--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,13 +1,17 @@
+// action
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
 
+// action 생성 함수
 export const increase = () => ({ type: INCREASE });
 export const decrease = () => ({ type: DECREASE });
 
+// 초기 state
 const initalState = {
   number: 0,
 };
 
+// reducer
 function counter(state = initalState, action) {
   switch (action.type) {
     case INCREASE:
